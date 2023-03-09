79d4374db5e360498488752b8894a07a83e87648
Yashwanth Gudala <yashwanthgudala7@gmail.com>
2023-03-09 16:23:29 +0530

    Yashwanth Version 1.7 Added Metadata Data Access , Metadata Data Access Test

---
+++ MetadataDataAccessTest/DataAccessTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetadataDataAccessTest
+{
+    class DataAccessTest
+    {
+        static void Main(string[] args)
+        {
+            //testing add new code list
+           //TestMetadataDataAccess.AddCodeValueToListTest();
+
+            int option;
+            do
+            {
+                Console.WriteLine("---------------------");
+                Console.WriteLine("Meta Data Access");
+                Console.WriteLine("1. Add New Code Value List");
+                Console.WriteLine("2. View All Code Value List");
+                Console.WriteLine("3. Delete Code Value List");
+                Console.WriteLine("4. Add Code Value Pair To List");
+                Console.WriteLine("5. Exit Application");
+
+                option = Convert.ToInt32(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        TestMetadataDataAccess.AddNewCodeValueListTest();
+                        break;
+
+                    case 2:
+                        TestMetadataDataAccess.ViewAllCodeValueListsTest();
+                        break;
+
+                    case 3:
+                        TestMetadataDataAccess.DeleteCodeValueListTest();
+                        break;
+                    case 4:
+                        TestMetadataDataAccess.AddCodeValueToListTest();
+                        break;
+                }
+            } while (option != 5);
+
+        }
+    }
+}
